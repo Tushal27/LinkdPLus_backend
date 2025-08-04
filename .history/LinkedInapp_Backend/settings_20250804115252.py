0aba6a7653e5b386c9b0b39feb6b6b26067e3277
Tushal <rajtushal74@gmail.com>
2025-08-04 13:43:32 +0530

    done

--- .history/LinkedInapp_Backend/settings_20250804115252.py
+++ .history/LinkedInapp_Backend/settings_20250804115252.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
-
+import dj_database_url
 import os
 from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ["LinkdPlus.railway.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["LinkdPlus.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
