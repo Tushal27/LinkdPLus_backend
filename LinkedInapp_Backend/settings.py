96eae892c83cbbce6698da5568de0b0349dbfb0e
Tushal <rajtushal74@gmail.com>
2025-08-04 12:43:15 +0530

    done adding Procfile

--- LinkedInapp_Backend/settings.py
+++ LinkedInapp_Backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('django-insecure--2kc193(kchj4j*3szt717rke+k7p5!9b+$&boutms09=ae65o')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
